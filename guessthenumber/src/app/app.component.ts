82a5a270a40e918ff73a9f0d0edf8ac6de794a2f
Sean Coleman <coleman07@gmail.com>
2019-12-08 10:23:50 -0700

    Guess the number done

--- guessthenumber/src/app/app.component.ts
+++ guessthenumber/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'guessthenumber';
+  deviation: number;
+  noOfTries: number;
+  original: number;
+  guess: number;
+
+  constructor() {
+      this.initializeGame();
+  }
+  initializeGame() {
+      this.noOfTries = 0;
+      this.original = Math.floor((Math.random() * 1000) + 1);
+      this.guess = null;
+      this.deviation = null;
+  }
+  verifyGuess() {
+      this.deviation = this.original - this.guess;
+      this.noOfTries = this.noOfTries + 1;
+  }
 }
